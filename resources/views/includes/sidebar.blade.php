c28d3ea030d81d53d8a2e247ac5980b497f923d2
Phurpa tshering <norvan012@gmail.com>
2017-03-31 09:28:53 +0600

    change

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -16,8 +16,7 @@
                 <li>
                     <ul class="nav nav-list nav-menu-list-style">
                      @if(session('user_id')==1)
-                        <li><a href="#" class="tree-toggle nav-header"><i class="pull-left glyphicon glyphicon-list"></i>&nbsp;&nbsp;&nbsp;&nbsp;<span>Master</span><span class="menu-collapsible-icon glyphicon glyphicon-chevron-right"></span></a>
-                            <ul class="nav nav-list tree bullets">
+                        <li> <ul class="nav">
                                 <li><a href="{{route('view_user')}}"><i class='pull-left glyphicon glyphicon-circle-arrow-right'></i>&nbsp;&nbsp;&nbsp;User</a></li>
                                 <li><a href="{{route('view_role')}}"><i class='pull-left glyphicon glyphicon-circle-arrow-right'></i>&nbsp;&nbsp;&nbsp;Role</a></li>
                                 <li><a href="{{route('country_master.index')}}"><i class='pull-left glyphicon glyphicon-circle-arrow-right'></i>&nbsp;&nbsp;&nbsp;<span>Country</span></a></li>
