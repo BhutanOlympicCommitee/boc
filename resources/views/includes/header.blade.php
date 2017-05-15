088f047c2545545c9c619a34cd59cb4783b62877
Phurpa tshering <norvan012@gmail.com>
2017-05-15 11:07:48 +0600

    chane

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -27,7 +27,7 @@
       <ul class="nav navbar-nav navbar-right">
         <!-- Authentication Links -->
         @if (Auth::guest())
-        <li><a><strong>[Home]</strong></a></li>
+        <li><a href="http://bhutanolympiccommittee.org/"><strong>[Home]</strong></a></li>
         @else
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
