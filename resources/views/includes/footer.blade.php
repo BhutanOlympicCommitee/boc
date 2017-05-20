fc46208931f953bfad8e89f6b8b1940e4d0557e5
Phurpa tshering <norvan012@gmail.com>
2017-05-20 12:28:38 +0600

    cha

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,12 +1,14 @@
-<nav class="navbar navbar-default navbar-fixed-bottom">
+<nav class="navbar navbar-fixed-bottom" style="background-color:#d3d3d3;">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">
          <footer role="contentinfo">
+          <div class="container text-center">
             <p style="text-align: center;">
                 Bhutan Olympic Committee:&copy; <?php echo date('Y');?>
                 All Right Reserved!
             </p>
+          </div>
         </footer>
       </a>
     </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,19 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-fixed-top" style="background-color:#66b9bf;">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Athlete Information System</a>
+      <a class="navbar-brand" href="#"><img src="../public/images/boc.png"></img></a>
     </div>
+    <div class="text">
+    <ul class="nav navbar-nav">
+    </br>
+    </br>
+      <li><span><h2><strong>Bhutan Olympic Athlete Information System</strong></h2></span></li>
+    </ul>
+    <div>
     <ul class="nav navbar-nav navbar-right">
        @if (Auth::guest())
+     </br>
+     </br>
         <li class="active"><a href="http://bhutanolympiccommittee.org/"><strong>[Home]</strong></a></li>
       @else
         <li class="dropdown">
@@ -30,59 +39,6 @@
     </ul>
   </div>
 </nav>
-{{-- <nav class="navbar nav-default navbar-fixed-top" style="background-color:#66b9bf">
-  <div class="container">
-    <div class="navbar-header">
-
-      <!-- Collapsed Hamburger -->
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-        <span class="sr-only">Toggle Navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <!-- Branding Image -->
-      <a class="navbar-brand navbar-collapse"> <img src="{{URL::asset('/images/boc_1.png')}}" alt="profile Pic" height="70px" width="600px"></img>
-      </a>
-    </div>
-    <br>
-    <br>
-  
-    <div class="collapse navbar-collapse" id="app-navbar-collapse">
-      <!-- Left Side Of Navbar -->
-      <ul class="nav navbar-nav">
-        &nbsp;
-      </ul>
-
-      <!-- Right Side Of Navbar -->
-      <ul class="nav navbar-nav navbar-right">
-        <!-- Authentication Links -->
-        @if (Auth::guest())
-        <li><a href="http://bhutanolympiccommittee.org/"><strong>[Home]</strong></a></li>
-        @else
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-            <b>{{ Auth::user()->emp_id }}</b> <span class="caret"></span>
-          </a>
-
-          <ul class="dropdown-menu" role="menu">
-            <li>
-              <a href="{{ url('/logout') }}"
-              onclick="event.preventDefault();
-              document.getElementById('logout-form').submit();">
-              <b>Logout</b>
-            </a>
-
-            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-              {{csrf_field()}}
-            </form>
-          </li>
-        </ul>
-      </li>
-      @endif
-    </ul>
-  </div>
-</div>
-</nav>
-<br>
-<br> --}}
+</br>
+</br>
+</br>
